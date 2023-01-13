c6b82f992a980e28fa52821d7b9d56114370d974
PiotrWolniik <w.peter@interia.eu>
2023-01-13 20:27:05 +0100

    Final

--- Lab_13_TDD/app.py
+++ Lab_13_TDD/app.py
@@ -1,11 +1,6 @@
 from typing import List
 
-class GetAttr(type):
-    def __getitem__(cls, x):
-        return getattr(cls, x)
-
 class Person:
-    __metaclass__ = GetAttr
     def __init__(self, name: str = "Piotr", surname: str = "Wolnik", age: int = 22):
         self.name = name
         self.surname = surname
@@ -14,29 +9,20 @@ class Person:
     def __eq__(self, __o: object) -> bool:
         return self.name == __o.name and self.surname == __o.surname and self.age == __o.age
 
+    def __str__(self):
+        return f"{self.surname}, {self.name} -> {self.age}"
 
 class People:
     def __init__(self, group_of_people: list):
-        self.people = group_of_people
+        self.people = []
+        for person in group_of_people:
+            self.people.append(person)
 
     def __getitem__(self, key):
         return self.people[key]
 
-# class PersonClassIter:    
-    
-#     def __init__(self, PeopleGroup):
-#         self._group_of_people = PeopleGroup.people
-#         self._group_of_people_size = len(self._group_of_people)
-#         self._current_index = 0    
-        
-#         def __iter__(self):
-#             return self    
-        
-#         def __next__(self):
-#             if self._current_index < self._group_of_people_size:
-#                 person = self._lect[self._current_index] 
-#             else:
-#                 person = self._group_of_people[self._current_index]
-#                 self._current_index += 1
-#             return person        
-#             raise StopIteration
+    def __str__(self):
+        string = ""
+        for person in self.people:
+            string += f"{person}\n"
+        return string
--- Lab_13_TDD/test/test_person.py
+++ Lab_13_TDD/test/test_person.py
@@ -13,12 +13,17 @@ testdata1 = [
         Person("Joanna", "Phalaphel", 23)
 ]
 
-@pytest.mark.parametrize('sample', testdata1)
-def test_People_constructor(sample):
-    people = People(sample)
+def test_People_constructor():
+    people = People(testdata1)
     assert people[0] == Person("Piotr", "Wolnik", 22)
     assert people[1] == Person("Michael", "Crazy", 24)
     assert people[2] == Person("Joanna", "Phalaphel", 23)
 
 
-
+testdata = ["Wolnik, Piotr -> 22", "Crazy, Michael -> 24"]
+@pytest.mark.parametrize('sample', testdata)
+def test_str_method(sample):
+    people = People([Person("Piotr", "Wolnik", 22), Person("Michael", "Crazy", 24)])
+    assert str(people[0]) == testdata[0]
+    assert str(people[1]) == testdata[1]
+    
