477d896460a2ae0126d63214d213c190357ab174
PiotrWolniik <w.peter@interia.eu>
2023-01-22 17:53:53 +0100

    More changes

--- Lab14/lab14.r
+++ Lab14/lab14.r
@@ -1,5 +1,45 @@
+# Zadanie 1 PIPES
 # install.packages("magrittr")
 library(magrittr)
+library(dplyr)
 
-lista = 1:10;
+lista = 1:10
 
+lista %<>% log2()  %>% sin() %>% sum() %>% sqrt()
+print(lista)
+
+data(iris)
+print(head(iris))
+aggregated_iris <- iris %>% aggregate(. ~Species, ., mean)
+
+
+# Zadanie 2 PLOT
+# install.packages("ggplot2 ")
+library(ggplot2)
+my_plot <- ggplot(data = iris, aes(x = Sepal.Width)) + geom_histogram(aes(fill=Species, color=Species), bins = 18) + geom_vline(data=aggregated_iris, aes(xintercept=Sepal.Width, color=Species), linetype="dashed") + labs(x='x', y='y', title='title')
+ggsave("~/Desktop/AiBD/Piotr_Wolnik_AiBD/Lab14/plot1.jpg", plot = my_plot)
+
+library("GGally")
+my_plot_2 <- ggpairs(data = iris, aes(color=Species))
+ggsave("~/Desktop/AiBD/Piotr_Wolnik_AiBD/Lab14/plot2.jpg", plot=my_plot_2)
+
+
+# Zadanie 3
+library(cluster)
+x <- iris[,1:4]
+y <- iris[, 5]
+
+# Here via c() function we create vector
+sum_sqr <- c()
+
+for (i in 1:10){
+    kmeans_res = kmeans(x, i)
+    sum_sqr <- append(sum_sqr, kmeans_res$tot.withinss) # where tot. withinss : Total within-cluster sum of squares
+}
+
+my_plot3 = ggplot(data.frame(iteration=1:length(sum_sqr), value=sum_sqr), aes(x=iteration, y=sum_sqr)) + geom_line()
+ggsave("~/Desktop/AiBD/Piotr_Wolnik_AiBD/Lab14/plot3.jpg", plot=my_plot3)
+
+kmeans_res <- kmeans(x, 3)
+my_plot4 <- ggplot(iris, aes(x = Sepal.Width, y = Petal.Width, color=kmeans_res$cluster)) + geom_point()
+ggsave("~/Desktop/AiBD/Piotr_Wolnik_AiBD/Lab14/plot4.jpg", plot=my_plot4)
